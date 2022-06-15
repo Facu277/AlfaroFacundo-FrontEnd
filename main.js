87b12c52c995915f9cd7b589ea7a9e5a23f21b23
Facundo <facuu.a96@gmail.com>
2022-06-15 17:09:45 -0300

    Actualizacion de js

--- main.js
+++ main.js
@@ -3,16 +3,23 @@ var parrafo = document.getElementById("sobremi");
 var contenido = parrafo.innerText;
 
 cuadroTexto.classList.add("editarParrafo");
+cuadroTexto.id="edicion";
+
 function editarNombre(){
     cuadroTexto.innerHTML=contenido;
     parrafo.replaceWith(cuadroTexto);
 }
 
+function agregar(){
+    var p1 = document.getElementById("edicion").value;
+    contenido = p1;
+    parrafo.innerText=contenido;
+    cuadroTexto.replaceWith(parrafo);
+    /* alert(parrafo.innerHTML); */
+}
 
-function agregarContenido(){
-    var areatexto = document.getElementsByTagName("textarea");
-    var pr=document.getElementById("sobremi");
-    var text = areatexto.value;
-    pr.textContent=text;
-    areatexto.replaceWith(pr);
+
+var foto1 = document.getElementById("fotoPerfil");
+function editarFoto(){
+    document.getElementById("fotoPerfil").src="img/foto_perfil/imgProfile2.jpg"
 }
